47a4bc2225371da8f2972c6c4dcd5835818557e9
Brian Smith <briandsmith1@gmail.com>
2014-03-09 18:43:12 +0000

    line tracing View

---
+++ sandbox/pathFinder/src/com/example/pathfinder/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.pathfinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+	private static final String DEBUG = "app";
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		Log.d(DEBUG, "MainActivity onCreate");
+				
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
