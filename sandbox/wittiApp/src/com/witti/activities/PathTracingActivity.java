fe1416a60c1aacd84aae6cb22debd47f53b6106b
Brian Smith <briandsmith1@gmail.com>
2014-03-14 20:53:36 +0000

    Updated with path finder test. Added yxzPoints.txt to res/raw folder inside the app project.

---
+++ sandbox/wittiApp/src/com/witti/activities/PathTracingActivity.java
@@ -0,0 +1,18 @@
+package com.witti.activities;
+
+import com.witti.wittiapp.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PathTracingActivity extends Activity {
+
+	 @Override
+	    protected void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+	        setContentView(R.layout.activity_pathtracing);
+	    }
+	 
+	
+}
