337954df33e9a58de229200ade1a448943b0abae
Alex Warren <amwarren@email.arizona.edu>
2014-04-15 18:13:55 +0000

    removed bin gen and .settings

--- sandbox/wittiApp/res/raw/convert_to_binary.py
+++ sandbox/wittiApp/res/raw/convert_to_binary.py
@@ -8,22 +8,30 @@ from sys import argv
 from array import array
 from random import random
 
-RANDOM_THRESHOLD = .8
+RANDOM_THRESHOLD = 0
 
 def main():
     """
-    Main function that converts from txt to binary,
-    given command line args.
+    Main function that allows this module to
+    operate as a script.
     """
     if len(argv) != 3:
         print "Usage ./convert_to_binary.py [input] [output]"
-    in_file_name = argv[1]
-    out_file_name = argv[2]
+    create_binary_frames(argv[1], argv[2], 10000)
+
+def create_binary_frames(in_file_name, out_file_name, 
+            chunk_size = None, threshold = RANDOM_THRESHOLD):
+    """
+    Function that converts from txt to binary,
+    given command line args.
+    """
+    file_index = 0
+    out_file_name  = out_file_name + "_{:0>4}.bin"
     in_file = open(in_file_name, 'r')
-    out_file = open(out_file_name, 'wb')
+    out_file = open(out_file_name.format(file_index), 'wb')
     point_counter = 0
     for line in in_file:
-        if random() < RANDOM_THRESHOLD:
+        if random() < threshold:
             continue
         yxz = line.split()
         if len(yxz) != 3:
@@ -32,7 +40,13 @@ def main():
         point_counter += 1
         array_buffer = array('d', [float(x) for x in yxz])
         array_buffer.tofile(out_file)
-    print point_counter
+        if chunk_size and point_counter % chunk_size == 0:
+            out_file.close()
+            file_index += 1
+            out_file = open(out_file_name.format(file_index), 'wb')
+            
+    print "Points: {}".format(point_counter)
+    print "Files:  {}".format(file_index+1)
 
 
 if __name__ == "__main__":
