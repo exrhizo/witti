06118ff1915c23bee11bca92093b20df8d80480c
Brian Smith <briandsmith1@gmail.com>
2014-04-18 21:08:10 +0000

    updated CloudCamera.java and CloudSurfaceView.java for organizational and OO reasons

--- src/wittiApp/src/edu/arizona/ece473573/witti/activities/CloudCamera.java
+++ src/wittiApp/src/edu/arizona/ece473573/witti/activities/CloudCamera.java
@@ -7,24 +7,66 @@ package edu.arizona.ece473573.witti.activities;
 
 import android.opengl.Matrix;
 
+/**
+ * This class controls the camera matrix. It is the primary way of changing the current view 
+ * 
+ * 
+ */
 public class CloudCamera {
 	
     private float[] mViewMatrix;
+    float eyeX, eyeY;
+    float lookX, lookY, lookZ;
 
     public CloudCamera(){
     	
     	mViewMatrix = new float[16];
+
+        eyeX = 0f; eyeY = 0f;
+
+        lookX = 0.0f;
+        lookY = 20.0f;
+        lookZ = 10.0f;
     	
     }
     
     public float[] getViewMatrix(){
     	return mViewMatrix;
     }
-    
+
+    /**
+     * This is a helper function to call the setLookAtM which updates
+     * the current camera matrix
+     * 
+     * @param 	eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ: float values
+     * 			for current camera matrix
+     */
     public void setCamera(float eyeX, float eyeY, float eyeZ, 
             float lookX, float lookY, float lookZ, 
             float upX, float upY, float upZ) {
     		
     	Matrix.setLookAtM(mViewMatrix, 0, eyeX, eyeY, eyeZ, lookX, lookY, lookZ, upX, upY, upZ);
     }
+
+    /**
+     * Sets the server web address to be used in Launch mode.
+     * 
+     * @param 	theta: The current angle calculated from the CloudDisplayView based on how far apart
+     * 					the motion events were
+     */
+	public void rotateCamera(float theta) {
+		
+		float tempX, tempY;
+		
+		//Simple rotation matrix calculations to rotate about the Z-axis
+		tempX = (float) ((lookX * Math.cos(theta)) - (lookY * Math.sin(theta)));
+		tempY = (float) ((lookX * Math.sin(theta)) + (lookY * Math.cos(theta)));
+		lookX = tempX;
+		lookY = tempY;
+
+		this.setCamera(0.0f, -10.0f, 10.0f,
+				lookX, lookY, lookZ,
+                0.0f,   0.0f,  1.0f);
+		
+	}
 }
--- src/wittiApp/src/edu/arizona/ece473573/witti/cloudview/CloudSurfaceView.java
+++ src/wittiApp/src/edu/arizona/ece473573/witti/cloudview/CloudSurfaceView.java
@@ -11,35 +11,22 @@ import android.content.Context;
 import android.opengl.GLSurfaceView;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
 
 public class CloudSurfaceView  extends GLSurfaceView{
 	
     private static final String CAT_TAG = "WITTI_CloudSurfaceView";
-    private static final String DEBUG = "DEBUG_TAG";
     CloudRenderer mRenderer;
     private CloudCamera mCamera;
     PointCloud mCloudPoints;
-    private GestureDetector mGestureDetector;
     
     //Touch vars
     float initialX, initialY;
     float prevX, prevY, currX, currY;
-    float eyeX, eyeY;
-    float lookX, lookY, lookZ;
     float theta;
-    float LENGTH = 100f;
 
     public CloudSurfaceView(Context context, AttributeSet attrs) {
         super(context, attrs);
-
-        mGestureDetector = new GestureDetector(getContext(), new CameraGestureListener());
-        eyeX = 0f; eyeY = 0f;
-
-        lookX = 0.0f;
-        lookY = 20.0f;
-        lookZ = 10.0f;
         
         initialize();
     }
@@ -47,7 +34,6 @@ public class CloudSurfaceView  extends GLSurfaceView{
     public CloudSurfaceView(Context context) {
         super(context);
 
-        mGestureDetector = new GestureDetector(getContext(), new CameraGestureListener());
         
         initialize();
     }
@@ -61,17 +47,25 @@ public class CloudSurfaceView  extends GLSurfaceView{
     private void initialize(){
         Log.v(CAT_TAG, "CloudRenderer initialize");
     }
-    
+
+    /**
+     * The touch events are used primarily for rotating the camera
+     * 
+     */
     @Override
     public boolean onTouchEvent(MotionEvent me){
     	super.onTouchEvent(me);
-    	
-    	float tempX;
-    	float tempY;
-    	
+    	    	
+    	/*
+    	 * When a user begins to drag their fingers across the screen,
+    	 * the distance between each point is used to calcuate an angle,
+    	 * the same way a segment of a circle maps out an angle. The LENGTH
+    	 * constant is used as the radius
+    	 */
     	switch(me.getAction()){
     	case MotionEvent.ACTION_UP:
-    		break;
+    		break; 	//Nothing done since the prev values are reset on a new
+    				//Motion Down anyways
     	case MotionEvent.ACTION_DOWN:
     		prevX = initialX = me.getX();
     		prevY = initialY = me.getY();
@@ -81,18 +75,11 @@ public class CloudSurfaceView  extends GLSurfaceView{
     		currY = me.getY();
     		
     		//theta = (float) Math.atan((currX-prevX)/LENGTH);
+    		//Much simpler angle calculation
     		theta = (currX-prevX) / 360;
     		
-    		tempX = (float) ((lookX * Math.cos(theta)) - (lookY * Math.sin(theta)));
-    		tempY = (float) ((lookX * Math.sin(theta)) + (lookY * Math.cos(theta)));
-    		lookX = tempX;
-    		lookY = tempY;
+    		mCamera.rotateCamera(theta);
     		
-    		Log.d(DEBUG, "LookX: " + lookX + " LookY: " + lookY + " theta: " + theta);
-    		
-    		mCamera.setCamera(0.0f, -10.0f, 10.0f,
-    				lookX, lookY, lookZ,
-                    0.0f,   0.0f,  1.0f);
     		prevX = currX;
     		prevY = currY;
     		break;
@@ -101,11 +88,6 @@ public class CloudSurfaceView  extends GLSurfaceView{
     			
     	}
     	
-    	//mGestureDetector.onTouchEvent(me);
-    	/*
-    	mCamera.setCamera((float) (10*Math.cos(mRenderer.mTime)), (float) (10*Math.sin(mRenderer.mTime)), (float) (5+5*Math.sin(.01*mRenderer.mTime)),
-                0.0f,   0.0f,  0.0f,
-                0.0f,   0.0f,  1.0f);*/
     	return true;
     	
     }
@@ -114,46 +96,4 @@ public class CloudSurfaceView  extends GLSurfaceView{
     	mCamera = cc;
     }
     
-    private class CameraGestureListener implements GestureDetector.OnGestureListener {
-
-    	@Override
-    	public boolean onDown(MotionEvent arg0) {
-    		// TODO Auto-generated method stub
-    		return false;
-    	}
-
-    	@Override
-    	public boolean onFling(MotionEvent arg0, MotionEvent arg1, float arg2,
-    			float arg3) {
-    		return false;
-    	}
-
-    	@Override
-    	public void onLongPress(MotionEvent arg0) {
-    		// TODO Auto-generated method stub
-    		
-    	}
-
-    	@Override
-    	public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float arg2,
-    			float arg3) {
-    		// TODO Auto-generated method stub
-    		return false;
-    	}
-
-    	@Override
-    	public void onShowPress(MotionEvent arg0) {
-    		// TODO Auto-generated method stub
-    		
-    	}
-
-    	@Override
-    	public boolean onSingleTapUp(MotionEvent arg0) {
-    		mRenderer.mTime += 0.02;
-    		return true;
-    	}
-    	
-
-    }
-
 }
