6caa830030e2a02eaabef4ccd3be0f867277d0ac
Brianna Heersink <bmheersink@gmail.com>
2014-04-18 21:20:37 +0000

    Fixed About problems. Hopefully.

--- src/wittiApp/src/edu/arizona/ece473573/witti/activities/AboutActivity.java
+++ src/wittiApp/src/edu/arizona/ece473573/witti/activities/AboutActivity.java
@@ -1,13 +1,12 @@
 //ECE 573 Project
 //Team: Witty
-//Date: 4/17/14
+//Date: 4/18/14
 //Author: Brianna Heersink
 
 package edu.arizona.ece473573.witti.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-
 import edu.arizona.ece473573.witti.R;
 
 public class AboutActivity extends Activity {
@@ -15,7 +14,7 @@ public class AboutActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-       //setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_about);
 
     }
 
